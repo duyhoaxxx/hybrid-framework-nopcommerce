fea2646c311fc550d981346e0c5e12533eafcb5f
duyhoa <kane.pham@kohyoung.com>
2022-01-12 17:24:28 +0700

    Java Basic

--- javaTester/javaBasic/Topic_01_Data_Type.java
+++ javaTester/javaBasic/Topic_01_Data_Type.java
@@ -1,5 +1,11 @@
 package javaBasic;
 
-public class Topic_01_Data_Type {
+import org.testng.annotations.Test;
 
+public class Topic_01_Data_Type {
+	static int Number;
+   @Test
+	public void main() {
+		System.out.println(Number);
+	}
 }
